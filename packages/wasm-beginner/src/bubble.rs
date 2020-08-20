b4e6f66c299a5cce0802045f6d2e9b29cd53db40
wander <junjiehuang@yeah.net>
2020-08-20 14:06:50 +0800

    fix(bubble) rust bubble sort improve

--- packages/wasm-beginner/src/bubble.rs
+++ packages/wasm-beginner/src/bubble.rs
@@ -1,12 +1,64 @@
-pub fn run(arr: &mut Vec<i32>) -> &mut Vec<i32>  {
-  let len = arr.len();
-  for _i in 0..len {
-    for j in 0..len - 1 {
-      if arr[j] > arr[j + 1] {
-        arr.swap(j, j + 1);
+pub fn run(arr: &mut [i32]) ->&mut [i32]  {
+  let mut new_len: usize;
+  let mut len = arr.len();
+  loop {
+    new_len = 0;
+    for i in 1..len {
+      if arr[i - 1] > arr[i] {
+        arr.swap(i - 1, i);
+        new_len = i;
       }
     }
+    if new_len == 0 {
+      break;
+    }
+    len = new_len;
   }
 
   arr
+  // 另一种方式
+  // let len = arr.len();
+  // for _i in 0..len {
+  //   for j in 0..len - 1 {
+  //     if arr[j] > arr[j + 1] {
+  //       arr.swap(j, j + 1);
+  //     }
+  //   }
+  // }
+
+  // arr
+}
+
+#[cfg(test)]
+mod tests {
+  use rand::seq::SliceRandom;
+  use rand::thread_rng;
+  use std::time::{SystemTime};
+
+  fn shuffle(arr: &mut Vec<i32>) -> Vec<i32> {
+    let mut rng = thread_rng();
+    arr.shuffle(&mut rng);
+    arr.to_vec()
+  }
+  #[test]
+  fn bubble_test() {
+    let len = 20000;
+    let mut arr: Vec<i32> = Vec::new();
+    let mut i = 0;
+    loop {
+      arr.push(i);
+      i += 1;
+      if  i == len {
+        break;
+      }
+    }
+
+    let mut input = shuffle(&mut arr);
+    let before = SystemTime::now();
+
+    println!("=========================== start ======================");
+
+    crate::bubble::run(&mut input).to_vec();
+    println!("duration {:?}", SystemTime::now().duration_since(before).unwrap());
+  }
 }
