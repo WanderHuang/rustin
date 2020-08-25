beffbc07aca0e929164ef5ae7212f8997b9ba6c5
wander <junjiehuang@yeah.net>
2020-08-25 18:06:18 +0800

    feature(md5) add project package: wasm-md5

---
+++ packages/wasm-md5/src/lib.rs
@@ -0,0 +1,14 @@
+use md5::{compute};
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen(js_name="rustEncrypt")]
+pub fn md5(strs: String) -> String {
+    format!("{:x}", compute(strs))
+}
